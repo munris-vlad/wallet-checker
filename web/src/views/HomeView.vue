33434955b4bb4a44c2204dfa28ca9d44e9f9439a
munris <>
2023-09-17 01:19:32 +0300

    home fix

--- web/src/views/HomeView.vue
+++ web/src/views/HomeView.vue
@@ -4,11 +4,16 @@
             <h1 class="text-3xl">Wallet checker by Munris</h1>
 
             <div class="pt-5 pb-5">
-                Для работы чекера zkSync/Starknet/Zora/Aptos/Linea нужно добавить адреса в папке addresses в соответствующие файлы.<br>
-                Для работы EVM чекера, переименовываем .env.example в .env и заполняем MORALIS_API_KEY. Для баланс чекера добавляем ANKR_API_KEY в .env
+                To work with the zkSync/Starknet/Zora/Aptos/Linea checker, you need to add addresses to the addresses folder in the respective files.<br>
+                To work with the EVM checker, rename .env.example to .env and fill in the MORALIS_API_KEY.
             </div>
 
-            <h2 class="text-3xl">Кошельки загружены</h2>
+            <div class="pt-5 pb-5">
+                Для работы чекера zkSync/Starknet/Zora/Aptos/Linea нужно добавить адреса в папке addresses в соответствующие файлы.<br>
+                Для работы EVM чекера, переименовываем .env.example в .env и заполняем MORALIS_API_KEY
+            </div>
+
+            <h2 class="text-3xl">Wallets loaded</h2>
 
             <ul class="pt-5">
                 <li class="pt-2">ZkSync: {{ data.zksync_wallets.length }}</li>
@@ -16,12 +21,12 @@
                 <li class="pt-2">Zora: {{ data.zora_wallets.length }}</li>
                 <li class="pt-2">Aptos: {{ data.aptos_wallets.length }}</li>
                 <li class="pt-2">Linea: {{ data.linea_wallets.length }}</li>
-                <li class="pt-2">EVM / Балансы: {{ data.evm_wallets.length }}</li>
+                <li class="pt-2">EVM / Balances: {{ data.evm_wallets.length }}</li>
             </ul>
 
             <ul class="pt-5">
                 <li class="pt-2">Author: <a href="https://t.me/munris_vlad" class="text-green-500 hover:text-green-600" target="_blank">Munris</a></li>
-                <li class="pt-2">Канал: <a href="https://t.me/munris_scripts" class="text-green-500 hover:text-green-600" target="_blank">Скрипты от Munris</a></li>
+                <li class="pt-2">Channel: <a href="https://t.me/munris_scripts" class="text-green-500 hover:text-green-600" target="_blank">Скрипты от Munris</a></li>
                 <li class="pt-2">Donate: munris.eth</li>
             </ul>
         </div>
