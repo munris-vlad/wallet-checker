bcbbb60123af3a4a4daf6cf2c35413143259e1e8
munris <>
2023-09-23 00:07:58 +0300

    fix

--- checkers/balances.js
+++ checkers/balances.js
@@ -86,7 +86,7 @@ const networks = {
             decimals: 6
         },
         'USDC': {
-            address: '0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8',
+            address: '0x2791bca1f2de4661ed88a30c99a7a9449aa84174',
             decimals: 18
         },
         'DAI': {
