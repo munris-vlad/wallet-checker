8ab59222d85c20959534b2ab1264c1fb98cf05d9
munris <>
2023-10-04 17:23:49 +0300

    fix menu start again

--- index.js
+++ index.js
@@ -30,8 +30,11 @@ function startExpressServer() {
 }
 
 async function startMenu(menu) {
+    let startOver = true
     if (menu === undefined) {
         mode = await entryPoint()
+    } else {
+        startOver = false
     }
 
     switch (mode) {
@@ -40,55 +43,55 @@ async function startMenu(menu) {
             break
         case "zksync":
             await zkSyncFetchDataAndPrintTable().catch(error => { console.error('Произошла ошибка:', error)})
-            await startMenu()
+            if (startOver) await startMenu()
             break
         case "starknet":
             await starknetFetchDataAndPrintTable().catch(error => { console.error('Произошла ошибка:', error)})
-            await startMenu()
+            if (startOver) await startMenu()
             break
         case "layerzero":
             await layerzeroFetchDataAndPrintTable().catch(error => { console.error('Произошла ошибка:', error)})
-            await startMenu()
+            if (startOver) await startMenu()
             break
         case "zora":
             await zoraFetchDataAndPrintTable().catch(error => { console.error('Произошла ошибка:', error)})
-            await startMenu()
+            if (startOver) await startMenu()
             break
         case "base":
             await baseFetchDataAndPrintTable().catch(error => { console.error('Произошла ошибка:', error)})
-            await startMenu()
+            if (startOver) await startMenu()
             break
         case "aptos":
             await aptosFetchDataAndPrintTable().catch(error => { console.error('Произошла ошибка:', error)})
-            await startMenu()
+            if (startOver) await startMenu()
             break
         case "linea":
             await lineaFetchDataAndPrintTable().catch(error => { console.error('Произошла ошибка:', error)})
-            await startMenu()
+            if (startOver) await startMenu()
             break
         case "evm":
             let chain = await evmNetwork()
             await evmFetchDataAndPrintTable(chain).catch(error => { console.error('Произошла ошибка:', error)})
-            await startMenu()
+            if (startOver) await startMenu()
             break
         case "balances":
             let network = await chooiceNetwork()
             await balancesFetchDataAndPrintTable(network).catch(error => { console.error('Произошла ошибка:', error)})
-            await startMenu()
+            if (startOver) await startMenu()
             break
         case "filter":
             await filterFetchDataAndPrintTable().catch(error => { console.error('Произошла ошибка:', error)})
-            await startMenu()
+            if (startOver) await startMenu()
             break
         case "nft":
             await nftFetchDataAndPrintTable().catch(error => { console.error('Произошла ошибка:', error)})
-            await startMenu()
+            if (startOver) await startMenu()
             break
     }
 }
 
 const args = process.argv.slice(2)
-const mode = args[0]
+let mode = args[0]
 
 if (mode === 'web') {
     startExpressServer()
